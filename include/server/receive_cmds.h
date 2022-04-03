8484e80794dd3bf2116574f9dc6778462181208c
Guilherme <guilherme.mecatronica@protonmail.com>
2022-04-03 19:38:56 +0100

    Add files via upload

--- include/server/receive_cmds.h
+++ include/server/receive_cmds.h
@@ -1,14 +1,21 @@
-#ifndef RECEIVE_CMDS_H
-#define RECEIVE_CMDS_H 1
-#include <sys/types.h>
-
-void recv_cmd(int sock);
-extern void create_file(int sock);
-void create_directory(int sock);
-void change_directory(int sock);
-void list_directory_contents(int sock, ssize_t dir);
-void file_info(int sock);
-void remove_file(int sock);
-void receive_file(int sock);
-void cat_file(int sock);
-#endif
+//MIT License
+//
+//Copyright (c) 2022 guu1
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
